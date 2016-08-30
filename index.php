92e5165d4ec42c6528f3833b128a3cceaaf33de7
Jhobanny Morillo <geomorillo@yahoo.com>
2016-08-29 22:30:17 -0500

    1.0alpha

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
 <?php
-
-/*
-session_start();
- 
-define( "APP_PATH", dirname( __FILE__ ) ."/app/" );
-
- use system\core\Registro;
-
-require_once('system/core/Registro.php');
-$registry = Registro::singleton();
-$registry->storeCoreObjects();
-//imprime la version
-print $registry->getFrameworkName();
- 
-exit();
-*/
 require_once 'system/core/Boot.php';
 use system\core\Boot;
 Boot::run();
