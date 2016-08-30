4234ceb9f323ced972d06bc1a3fc3c1d9d23603b
Daniel Navarro <danielnavram@gmail.com>
2016-08-30 13:51:43 -0500

    Routing Test

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 
 require_once 'system/core/Boot.php';
 use system\core\Boot;
+
 Boot::run();
+
+$router = new system\core\Router();
+$router->addRoute("/(UsuariosController)/(index)", array("controller", "method"));
+$router->addRoute("/(.*)", array("catchall"));
+print_r($router->parse($_SERVER['REQUEST_URI']));
