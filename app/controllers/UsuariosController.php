c8d72bf83bcc8f7a3ed3bcbf0a3b39e0741dcb90
Jhobanny Morillo <geomorillo@yahoo.com>
2016-08-31 21:58:52 -0500

    Added logger class

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -5,19 +5,30 @@
  * 2015
  * Contacto: geomorillo@yahoo.com
  */
+
 namespace app\controllers;
+
 use system\core\Controller;
 use system\core\View;
-class UsuariosController extends Controller{
+use system\core\Logger;
+
+class UsuariosController extends Controller
+{
 
     public function index()
     {
         $userModel = new \app\models\UserModel();
 
         $users = $userModel->getUsers();
-        
+
         $view = new View("usuarios", array("users" => $users));
         echo $view->render();
+    }
+
+    public function testLog()
+    {
+        Logger::alert("Ok");
+        Logger::info("{usuario} tiene {edad}", array("usuario" => "geo", "edad" => "36"));
     }
 
 }
---
+++ system/core/LogLevel.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Todos los derechos reservados por Manuel Jhobanny Morillo Ordoñez 
+ * 2015
+ * Contacto: geomorillo@yahoo.com
+ */
+
+namespace system\core;
+
+/**
+ * Description of LogLevel
+ *
+ * @author geomorillo
+ */
+/**
+ * Describes log levels
+ */
+class LogLevel
+{
+    const EMERGENCY = 'EMERGENCY';
+    const ALERT     = 'ALERT';
+    const CRITICAL  = 'CRITICAL';
+    const ERROR     = 'ERROR';
+    const WARNING   = 'WARNING';
+    const NOTICE    = 'NOTICE';
+    const INFO      = 'INFO';
+    const DEBUG     = 'DEBUG';
+}
