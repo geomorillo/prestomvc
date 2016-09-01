3583358cd3adcdcae98e16f87b3f09442df83dcd
Jhobanny Morillo <geomorillo@yahoo.com>
2016-09-01 00:26:59 -0500

    add class LogException

---
+++ system/core/LogException.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Todos los derechos reservados por Manuel Jhobanny Morillo Ordoñez 
+ * 2015
+ * Contacto: geomorillo@yahoo.com
+ */
+
+namespace system\core;
+
+use system\core\Logger;
+
+class LogException extends \Exception
+{
+
+    public function logError()
+    {
+        $errorMsg = 'Error on line ' . $this->getLine() . ' in ' . $this->getFile() . ':' . $this->getTraceAsString();
+        Logger::error($errorMsg);
+        return $errorMsg;
+    }
+
+    public function errorMessage($message)
+    {
+        $errorMsg = 'Error on line ' . $this->getLine() . ' in ' . $this->getFile() . ': ' . $message;
+        Logger::error($errorMsg);
+        return $errorMsg;
+    }
+
+}
