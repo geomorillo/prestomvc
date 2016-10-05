b7bd9cbdd1af5df05757cd502130ffd2fdb7301c
Jhobanny Morillo <geomorillo@yahoo.com>
2016-10-05 12:30:02 -0500

    register + csrf

---
+++ system/core/Csrf.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * Todos los derechos reservados por Manuel Jhobanny Morillo Ordoñez 
+ * 2015
+ * Contacto: geomorillo@yahoo.com
+ */
+
+namespace system\core;
+
+/**
+ * Description of Csrf
+ *
+ * @author geomorillo
+ */
+use system\core\Encrypter;
+use system\core\LogException;
+
+class Csrf
+{
+
+    public static function generate()
+    {
+        if (!USE_SESSIONS) {
+            LogException::errorMessage("Sessions not enabled");
+            return FALSE;
+        }
+        //generate csrf
+        $token_id = base64_encode(Encrypter::get_random_bytes(32));
+        //store in sessions
+        static::save("token_id", $token_id);
+        
+        //return generated csrf
+        return $token_id;
+    }
+
+    public static function validate($token)
+    {
+        if (!USE_SESSIONS) {
+            LogException::errorMessage("Sessions not enabled");
+            return FALSE;
+        }
+        //validate csrf
+        //get stored csrf
+        $token_id = static::get_key("token_id");
+        //if equal then valid
+        if($token_id === $token){
+            return TRUE;
+        }else{
+            static::destroy($token);
+            return FALSE;
+        }
+    }
+
+    public static function save($key, $value)
+    {
+        if (isset($_SESSION)) {
+            $_SESSION[$key] = $value;
+        }
+    }
+
+    public static function destroy($key)
+    {
+        $_SESSION[$key] = ' ';
+        unset($_SESSION[$key]);
+    }
+
+    public static function get_key($key)
+    {
+        if (!isset($_SESSION[$key])) {
+            return FALSE;
+        }
+        return $_SESSION[$key];
+    }
+
+}
