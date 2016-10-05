2231b6c1c688aeee50924d6a9990c172a0a59d5c
Jhobanny Morillo <geomorillo@yahoo.com>
2016-10-05 13:09:40 -0500

    fix config

--- app/config/config.php
+++ app/config/config.php
@@ -7,8 +7,8 @@
  */
 define("DEFAULTCONTROLLER", 'Main');
 define("DEFAULTMETHOD", 'index');
-define("USE_SESSIONS", TRUE);
-define("ENABLE_DEBUG",FALSE);//TRUE = DEVELOPMENT, FALSE = PRODUCCION
+define("USE_SESSIONS", FALSE);
+define("ENABLE_DEBUG",TRUE);//TRUE = DEVELOPMENT, FALSE = PRODUCCION
 define('ENCRYPT_KEY', "IeT5aprlRw7pImmU73T89tNyk0cOSrkA");
 define("DEFAULT_LANG", "es");
 define("DB_PREFIX",""); //prefix for tables in database
