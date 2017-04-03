66a5cd043fda8419e37a3c43a2e4f1add29437c6
Jhobanny Morillo <geomorillo@yahoo.com>
2017-04-02 19:45:33 -0500

    Defaults middleware names
    Sets default middleware names

--- system/core/Router.php
+++ system/core/Router.php
@@ -13,27 +13,27 @@ use system\core\Route;
 class Router extends Route
 {
 
-    public function get($route, $action, $before = NULL, $after = NULL)
+    public function get($route, $action, $before = "before", $after = "after")
     {
         $this->addRoute('GET', $route, $action, $before, $after);
     }
 
-    public function post($route, $action, $before = NULL, $after = NULL)
+    public function post($route, $action, $before = "before", $after = "after")
     {
             $this->addRoute('POST', $route, $action, $before, $after);
     }
 
-    public function put($route, $action, $before = NULL, $after = NULL)
+    public function put($route, $action, $before = "before", $after = "after")
     {
             $this->addRoute('PUT', $route, $action, $before, $after);
     }
 
-    public function delete($route, $action, $before = NULL, $after = NULL)
+    public function delete($route, $action, $before = "before", $after = "after")
     {
             $this->addRoute('DELETE', $route, $action, $before, $after);
     }
 
-    public function any($route, $action, $before = NULL, $after = NULL)
+    public function any($route, $action, $before = "before", $after = "after")
     {
             $this->addRoute('ANY', $route, $action, $before, $after);
     }
