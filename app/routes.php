3a1c7b9e17687dbef128fa0600902ce395894e64
Jhobanny Morillo <geomorillo@yahoo.com>
2018-06-19 17:54:04 -0500

    Some cleanup on routes

--- app/routes.php
+++ app/routes.php
@@ -13,9 +13,11 @@ use system\core\View;
  * $router->get("login",'app\controllers\Main@login') or shorter version $router->get("/","Main@login")
  */
 $router->get("/","Main@index");
+/* TEST ROUTES
 $router->get("login","Main@login");
 $router->post("authenticate","Main@authenticate");
 $router->get("register","Main@register");
 $router->get("logout","Main@logout");
 $router->get("enc","Main@enc");
 $router->get("key","Main@key");
+ */
